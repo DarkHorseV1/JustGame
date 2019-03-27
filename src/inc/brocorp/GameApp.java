2c5f3d735bd2816872849a61ffe7bd3a1d915cd5
DarkHorseV1 <darkhorse63189@gmail.com>
2019-03-28 02:06:36 +0300

    add main menu

--- src/inc/brocorp/GameApp.java
+++ src/inc/brocorp/GameApp.java
@@ -1,30 +1,17 @@
 package inc.brocorp;
 
-import inc.brocorp.i18n.Language;
-
 import javafx.application.Application;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
-public class gameApp extends Application {
 
-    @FXML
-    private ComboBox<Language> langComboBox = new ComboBox<Language>();
-
-
+public class GameApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        ObservableList<Language> list = Language.getLanguageList();
-        langComboBox.setItems(list);
-        langComboBox.getSelectionModel().select(1);
-
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/gameApp.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/GameApp.fxml"));
         primaryStage.setTitle("Just a game");
         Scene scene = new Scene(root,240, 250);
         primaryStage.setScene(scene);
--- src/inc/brocorp/controller/MainController.java
+++ src/inc/brocorp/controller/MainController.java
@@ -1,6 +1,42 @@
 package inc.brocorp.controller;
 
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class MainController {
+
+     public void showMenu(ActionEvent ae){
+        Object source = ae.getSource();
+        if(!(source instanceof Button))
+            return;
+        Button clickedButton = (Button)source;
+        if(clickedButton.getId().equals("btnRu"))
+            System.out.println("rus button");
+        else if(clickedButton.getId().equals("btnEn"))
+            System.out.println("eng button");
+        createMenu();
+     }
+
+     private void createMenu(){
+         try{
+             Stage stage = new Stage();
+             Parent root = FXMLLoader.load(getClass().getResource("..//fxml/MainMenu.fxml"));
+             Scene scene = new Scene(root,350,375);
+             stage.setTitle("Main menu");
+             stage.setResizable(false);
+             stage.setScene(scene);
+             stage.show();
+         }
+         catch(IOException e){
+            e.printStackTrace();
+         }
+     }
 
 }
