9aecbbc27de853c9df7c64bfac29482ad4ec544c
DarkHorseV1 <darkhorse63189@gmail.com>
2019-09-13 21:43:25 +0300

    add scaling animation

--- src/inc/brocorp/interfaceCange/ButtonScaling.java
+++ src/inc/brocorp/interfaceCange/ButtonScaling.java
@@ -1,8 +1,10 @@
 package inc.brocorp.interfaceCange;
 
+import javafx.animation.ScaleTransition;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.util.Duration;
 
 public class ButtonScaling {
 
@@ -14,13 +16,19 @@ public class ButtonScaling {
     }
 
     public static void setButtonScale(Button btn) {
-        btn.setOnMouseEntered(eh -> {
-            btn.setScaleX(1.08);
-            btn.setScaleY(1.08);
+        btn.setOnMouseEntered(event -> {
+            ScaleTransition st = new ScaleTransition(Duration.seconds(0.2),btn);
+            st.setToX(1.15);
+            st.setToY(1.15);
+            st.play();
         });
-        btn.setOnMouseExited(eh -> {
-            btn.setScaleX(1);
-            btn.setScaleY(1);
+        btn.setOnMouseExited(event -> {
+            ScaleTransition st = new ScaleTransition(Duration.seconds(0.2), btn);
+            st.setFromX(1.15);
+            st.setFromY(1.15);
+            st.setToX(1);
+            st.setToY(1);
+            st.play();
         });
     }
 }
